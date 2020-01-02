36ebbe4b4d56af6bc747d79e272534494c4630f9
Jaewoo Nam <namssabu12@gmail.com>
2020-01-02 11:58:21 +0900

    class

---
+++ New Unity Project/Assets/HelloClass.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloClass : MonoBehaviour
+{
+    void Start() {
+        Animal jack = new Animal();
+    }
+}
+
+public class Animal {
+    string name;
+    string sound;
+    float weight;
+
+}
