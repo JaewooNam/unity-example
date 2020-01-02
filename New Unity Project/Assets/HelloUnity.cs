bd6fa06c655d3f8b82569360d185c1d7340dc509
Jaewoo Nam <namssabu12@gmail.com>
2020-01-02 10:48:16 +0900

    C# Type

--- New Unity Project/Assets/HelloUnity.cs
+++ New Unity Project/Assets/HelloUnity.cs
@@ -8,11 +8,29 @@ public class HelloUnity : MonoBehaviour
     void Start()
     {
         Debug.Log("Hello World!");
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // 주석
+        /* */
+
+        // 소숫점 아래 7자리까지만 정확. 끝에 f를 붙인다.
+        // 32비트
+        float height = 0.1234567f;
+
+        // float의 두배의 메모리를 사용, 64비트
+        // 소숫점 아래 15자리 까지만 정확
+        double pi = 3.14159265359;
+        
+        bool isBoy = true;
+        bool isGirl = false;
+
+        char grade = 'A';
+
+        string movieTitle = "Matrix";
+
+        // var는 할당하는 값을 기준으로 타입을 결정
+        var myName = "JaewooNam";
+        // = string myName = "...."
+
         
     }
 }
